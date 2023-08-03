52f5e3782dc7077cea17ee202f19772b6597cfaf
Kabir Deula <kabirdeula167@gmail.com>
2023-08-03 17:11:03 +0545

    Update gundruk_spider.py

--- gundruk_spider.py
+++ gundruk_spider.py
@@ -2,13 +2,13 @@ import scrapy
 import re
 
 class GundukSpider(scrapy.Spider):
-    name = 'gunduk'
+    name = 'kiwi'
     start_urls = ['https://kalimatimarket.gov.np/price'] # replace with the URL of the website to scrape
 
     def parse(self, response):
-        price_min = response.css('#commodityPriceParticular td:contains("गुन्दुक") + td + td').extract_first()
-        price_max = response.css('#commodityPriceParticular td:contains("गुन्दुक") + td + td + td').extract_first()
-        price_avg = response.css('#commodityPriceParticular td:contains("गुन्दुक") + td + td + td + td').extract_first()
+        price_min = response.css('#commodityPriceParticular td:contains("किवि") + td + td').extract_first()
+        price_max = response.css('#commodityPriceParticular td:contains("किवि") + td + td + td').extract_first()
+        price_avg = response.css('#commodityPriceParticular td:contains("किवि") + td + td + td + td').extract_first()
 
         # use regex to extract text content within <td> tags
         price_min = re.search(r'<td>(.*?)</td>', price_min).group(1)
@@ -16,7 +16,7 @@ class GundukSpider(scrapy.Spider):
         price_avg = re.search(r'<td>(.*?)</td>', price_avg).group(1)
 
         yield {
-            'item': 'गुन्दुक',
+            'item': 'किवि',
             'price_min': price_min,
             'price_max': price_max,
             'price_avg': price_avg
